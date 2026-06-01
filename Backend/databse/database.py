4c0fc78c9c95e0243da12808245bdc99cbd49a22
IgMayank <mkdogra1981@gmail.com>
2026-06-01 16:13:49 +0530

    sqlite->postgresql

--- Backend/databse/database.py
+++ Backend/databse/database.py
@@ -2,12 +2,31 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "sqlite:///./crop_monitor.db"
+# DATABASE_URL = "sqlite:///./crop_monitor.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread":False}
-)
+# engine = create_engine(
+#     DATABASE_URL,
+#     connect_args={"check_same_thread":False}
+# )
+
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL",
+    "sqlite:///./crop_monitor.db")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "poatgresql://",
+        1
+    )
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread" : False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocker = sessionmaker(
     autocommit = False,
